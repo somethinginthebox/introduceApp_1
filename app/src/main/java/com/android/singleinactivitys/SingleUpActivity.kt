8e3bd10a28e9ee02b51428b846e801dfcec27748
somethinginthebox <ha862888@gmail.com>
2023-08-24 15:56:57 +0900

    [COMPLETION] : 🎉SingleUpActivity 42 line 오타 fix

--- app/src/main/java/com/android/singleinactivitys/SingleUpActivity.kt
+++ app/src/main/java/com/android/singleinactivitys/SingleUpActivity.kt
@@ -39,7 +39,7 @@ class SingleUpActivity : AppCompatActivity() {
                 Toast.makeText(this, "회원가입성공!", Toast.LENGTH_SHORT).show()
 
                 val loginIntent = Intent(this, SingleInActivity::class.java)
-                loginIntent.putExtra("joinInputId", joinInputNameText)
+                loginIntent.putExtra("joinInputId", joinInputIdText)
                 loginIntent.putExtra("joinInputPw", joinInputPwText)
                 setResult(RESULT_OK, loginIntent)
                 finish()
